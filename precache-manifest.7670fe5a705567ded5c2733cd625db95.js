037365cf6f5182d325bbab6f064a9d8abbde3c86
Goran Ljaljić <gljaljic@gmail.com>
2019-09-18 18:38:46 +0200

    Updates

---
+++ precache-manifest.7670fe5a705567ded5c2733cd625db95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f2bb805c557a844c74a82c6584a8a3",
+    "url": "/guess-the-number/index.html"
+  },
+  {
+    "revision": "0f06f88aa562b177e9a1",
+    "url": "/guess-the-number/static/css/main.5438afb6.chunk.css"
+  },
+  {
+    "revision": "97bf07fb34943decc4c7",
+    "url": "/guess-the-number/static/js/2.95e95d3f.chunk.js"
+  },
+  {
+    "revision": "0f06f88aa562b177e9a1",
+    "url": "/guess-the-number/static/js/main.d40d86aa.chunk.js"
+  },
+  {
+    "revision": "2a19ef42d9e94cb313a2",
+    "url": "/guess-the-number/static/js/runtime~main.f2fb5971.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/guess-the-number/precache-manifest.374a5876a353645b980b3570c399f879.js"
+  "/guess-the-number/precache-manifest.7670fe5a705567ded5c2733cd625db95.js"
 );
 
 self.addEventListener('message', (event) => {
