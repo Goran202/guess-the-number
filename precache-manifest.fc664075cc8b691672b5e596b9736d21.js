41f628dd5389b953e1666914aae81088f42b3de3
Goran Ljaljić <gljaljic@gmail.com>
2019-09-18 12:22:03 +0200

    Updates

---
+++ precache-manifest.fc664075cc8b691672b5e596b9736d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a72429ebbbac08de0eeccb9332f6a4b",
+    "url": "/guess-the-number/index.html"
+  },
+  {
+    "revision": "7a5b863f019e96b330f3",
+    "url": "/guess-the-number/static/css/main.862466e7.chunk.css"
+  },
+  {
+    "revision": "d22e61d5810e85811bbf",
+    "url": "/guess-the-number/static/js/2.96c453d3.chunk.js"
+  },
+  {
+    "revision": "7a5b863f019e96b330f3",
+    "url": "/guess-the-number/static/js/main.5ba9efaf.chunk.js"
+  },
+  {
+    "revision": "2a19ef42d9e94cb313a2",
+    "url": "/guess-the-number/static/js/runtime~main.f2fb5971.js"
+  }
+]);
